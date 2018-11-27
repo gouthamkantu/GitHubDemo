0cd1fb2e5188a0d4eeac275fb24b334a09763a83
gouthamkantu <gouthamkantu@gmail.com>
2018-11-27 12:35:45 -0500

    basis 2 lists
    next you should do the methord

---
+++ arrayListPractice.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+public class arrayListPractice{
+
+//	public static void combine(ArrayList<Integer> list2)
+
+	public static void main(String[]args){
+	ArrayList<Integer> list = new ArrayList<Integer>();
+	list.add(1);
+	list.add(2);
+	list.add(3);
+	System.out.println(list);
+	ArrayList<Integer> list2 = new ArrayList<Integer>();
+	list2.add(4);
+	list2.add(5);
+	list2.add(6);
+	System.out.println(list2);
+
+	//System.out.println("Helloe world..");
+	}
+
+}
